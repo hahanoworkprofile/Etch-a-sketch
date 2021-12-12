bc0a44ffc363ad576da1e5832a85b39ddf5d56eb
cloud <terakorn.p2@gmail.com>
2021-12-12 18:09:54 +0700

    main functions, all done

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+//ui
+const board = document.querySelector('.board'); 
+const colorPicker = document.querySelector('input[type="color"]');
+const penButton = document.querySelector('button[data-action="pen"]') ;
+const eraserButton = document.querySelector('button[data-action="eraser"]');
+const rainbowButton = document.querySelector('button[data-action="rainbow"] ');
+const clearButton = document.querySelector('button[data-action="clear"]');
+//variables
 let size = 16; 
+let color = colorPicker.value;
 
-const board = document.querySelector('.board');
+//scripting
+setUp(16);
+colorPicker.addEventListener('change', e => color = e.target.value);
+penButton.addEventListener('click', e => color=colorPicker.value); 
+eraserButton.addEventListener('click', e => color = "white"); 
 
-for(let i=0; i<=size*size; i++) { 
-    let grid = document.createElement('div'); 
-    grid.classList.add('grid');
-    board.appendChild(grid);
-    console.log(i);
+//functioning
+function setUp(size) { 
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for(let i = 0; i<=size*size; i++) { 
+        let div = document.createElement('div'); 
+        div.classList.add('grid'); 
+        div.addEventListener('mouseenter', e => { 
+            e.target.style["background-color"] = color
+        } 
+        ); 
+        board.appendChild(div);
+    } 
 }
 
