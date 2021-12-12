00810eb4514e37320527b511fe28096403ceb3af
cloud <terakorn.p2@gmail.com>
2021-12-12 18:36:45 +0700

    finished

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const clearButton = document.querySelector('button[data-action="clear"]');
 //variables
 let size = 16; 
 let color = colorPicker.value;
-
+let mode ="pen";
 //scripting
 setUp(16);
 colorPicker.addEventListener('change', e => color = e.target.value);
-penButton.addEventListener('click', e => color=colorPicker.value); 
-eraserButton.addEventListener('click', e => color = "white"); 
-
+penButton.addEventListener('click', e => mode="pen"); 
+eraserButton.addEventListener('click', e => mode="eraser"); 
+rainbowButton.addEventListener('click', e=> mode="rainbow"); 
+clearButton.addEventListener('click', e=> document.querySelectorAll('.grid').forEach(div=> div.style.backgroundColor = 'white'));
 //functioning
 function setUp(size) { 
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -23,10 +24,20 @@ function setUp(size) {
     for(let i = 0; i<=size*size; i++) { 
         let div = document.createElement('div'); 
         div.classList.add('grid'); 
-        div.addEventListener('mouseenter', e => { 
-            e.target.style["background-color"] = color
-        } 
-        ); 
+        div.addEventListener('mouseenter', e => {
+           const randColor = () => Math.round(Math.random() *200 );
+           if(mode=="rainbow") { 
+                  e.target.style.backgroundColor = `rgb(${randColor()}, ${randColor()}, ${randColor()})`;
+                  console.log(e.target.style.backgroundColor);
+                  return;  
+            }
+            if(mode=="eraser") { 
+                e.target.style.backgroundColor = 'white';
+                return;
+            }
+            color = colorPicker.value;
+            e.target.style.backgroundColor = color;
+        });
         board.appendChild(div);
     } 
 }
